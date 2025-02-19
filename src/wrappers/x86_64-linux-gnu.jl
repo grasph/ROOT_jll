ada3a561ed167ba21970bcd148ba69d1cf269d67
Philippe Gras <philippe.gras@cern.ch>
2025-02-19 08:59:13 +0100

    ROOT_jll build 6.32.8+2

--- src/wrappers/x86_64-linux-gnu.jl
+++ src/wrappers/x86_64-linux-gnu.jl
@@ -6,7 +6,6 @@ using Xorg_libXpm_jll
 using Xorg_libXft_jll
 using VDT_jll
 using XRootD_jll
-using LAPACK_jll
 using Lz4_jll
 using FFTW_jll
 using Giflib_jll
@@ -30,15 +29,15 @@ using Xorg_libxkbfile_jll
 using Libglvnd_jll
 using GLU_jll
 using GLEW_jll
-using nlohmann_json_jll
 using CFITSIO_jll
 using oneTBB_jll
+using OpenBLAS32
 JLLWrappers.@generate_wrapper_header("ROOT")
 JLLWrappers.@declare_executable_product(root)
 JLLWrappers.@declare_executable_product(rootcling)
 JLLWrappers.@declare_executable_product(rootcling_stage1)
 function __init__()
-    JLLWrappers.@generate_init_header(Xorg_libX11_jll, Xorg_libXpm_jll, Xorg_libXft_jll, VDT_jll, XRootD_jll, LAPACK_jll, Lz4_jll, FFTW_jll, Giflib_jll, Zstd_jll, PCRE2_jll, Graphviz_jll, xxHash_jll, XZ_jll, Librsvg_jll, FreeType2_jll, Xorg_libICE_jll, Xorg_libSM_jll, Xorg_libXfixes_jll, Xorg_libXi_jll, Xorg_libXinerama_jll, Xorg_libXmu_jll, Xorg_libXt_jll, Xorg_libXtst_jll, Xorg_xcb_util_jll, Xorg_libxkbfile_jll, Libglvnd_jll, GLU_jll, GLEW_jll, nlohmann_json_jll, CFITSIO_jll, oneTBB_jll)
+    JLLWrappers.@generate_init_header(Xorg_libX11_jll, Xorg_libXpm_jll, Xorg_libXft_jll, VDT_jll, XRootD_jll, Lz4_jll, FFTW_jll, Giflib_jll, Zstd_jll, PCRE2_jll, Graphviz_jll, xxHash_jll, XZ_jll, Librsvg_jll, FreeType2_jll, Xorg_libICE_jll, Xorg_libSM_jll, Xorg_libXfixes_jll, Xorg_libXi_jll, Xorg_libXinerama_jll, Xorg_libXmu_jll, Xorg_libXt_jll, Xorg_libXtst_jll, Xorg_xcb_util_jll, Xorg_libxkbfile_jll, Libglvnd_jll, GLU_jll, GLEW_jll, CFITSIO_jll, oneTBB_jll, OpenBLAS32)
     JLLWrappers.@init_executable_product(
         root,
         "bin/root",
